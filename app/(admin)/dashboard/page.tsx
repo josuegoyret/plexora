1e2c854afe88e241624d40d2f67363e8ddf77c22
josuegoyret <fjgoyretsola@gmail.com>
2025-10-22 13:59:05 -0300

    Enhance DashboardPage to display access and refresh tokens
    - Added functionality to retrieve access and refresh tokens from search parameters.
    - Introduced Card component to display token information when available.
    - Wrapped the main content in a <main> tag for better semantic structure.

--- app/(admin)/dashboard/page.tsx
+++ app/(admin)/dashboard/page.tsx
@@ -2,9 +2,26 @@
 
 import { connectWixSite } from "@/actions/wix";
 import { Button } from "@/components/ui/button";
+import { Card, CardContent } from "@/components/ui/card";
+import { useSearchParams } from "next/navigation";
 
 const DashboardPage = () => {
-  return <Button onClick={connectWixSite}>Connect My Wix Site</Button>;
+  const searchParams = useSearchParams();
+  const accessToken = searchParams.get("access_token");
+  const refreshToken = searchParams.get("refresh_token");
+
+  return (
+    <main>
+      <Button onClick={connectWixSite}>Connect My Wix Site</Button>
+      {accessToken && refreshToken && (
+        <Card>
+          <CardContent>
+            <pre>{JSON.stringify({ accessToken, refreshToken }, null, 2)}</pre>
+          </CardContent>
+        </Card>
+      )}
+    </main>
+  );
 };
 
 export default DashboardPage;
